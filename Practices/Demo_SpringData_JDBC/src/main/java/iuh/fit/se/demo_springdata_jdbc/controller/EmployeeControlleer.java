8667ac776c499bcc59c4f010af46f6cd5844f24c
TranCongMinh14 <tcminhangiang@gmail.com>
2025-10-05 10:57:08 +0700

    feat: Initialize Demo_SpringData_JPA, Demo_SpringData_JDBC and Demo_SpringData_MongoDB projects

---
+++ Practices/Demo_SpringData_JDBC/src/main/java/iuh/fit/se/demo_springdata_jdbc/controller/EmployeeControlleer.java
@@ -0,0 +1,4 @@
+package iuh.fit.se.demo_springdata_jdbc.controller;
+
+public class EmployeeControlleer {
+}
---
+++ Practices/Demo_SpringData_JDBC/src/test/java/iuh/fit/se/demo_springdata_jdbc/DemoSpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.demo_springdata_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringDataJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
